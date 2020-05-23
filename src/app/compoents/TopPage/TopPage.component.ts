58fa434e27a1d9edf009efe15c275a1083189fe3
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-05-23 10:53:21 +0900

    トップページのコンポーネントの追加

---
+++ src/app/compoents/TopPage/TopPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-TopPage',
+  templateUrl: './TopPage.component.html',
+  styleUrls: ['./TopPage.component.css']
+})
+export class TopPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
