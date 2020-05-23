506e95ada3eb079ebc7f423d28852bd8e3a3aa0f
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-05-23 11:02:32 +0900

    bulmaの適用

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopPageComponent } from './compoents/TopPage/TopPage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TopPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
